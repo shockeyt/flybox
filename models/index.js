6abe491e5ac7b13f96e2ff411e10bec168ffb63c
shockeyt <shockeyt@gmail.com>
2017-05-02 14:04:38 -0600

    changed mongo db path

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 mongoose.connect( process.env.MONGODB_URI ||
 				  process.env.MONGOLAB_URI ||
 				  process.env.MONGOHQ_URL ||
-				  'mongodb://localhost/flybox');
+				  'mongodb://localhost/anotherflybox');
 
 module.exports.Fly = require('./fly.js');
